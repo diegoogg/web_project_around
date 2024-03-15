55d66126dc77d822d5f152743bc10eb62e7b8f0e
Diego Ocegueda <diego.ogg@hotmail.com>
2024-03-15 09:40:14 -0600

    primer deploy proyecto

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Tripleten web_project_around
+
+Sitio web de tripleten en el que se desarrollo utilizando las siguientes tecnologias:
+HTML
+CSS 
+JavaScript
+
+En el sitio web se presenta un perfil donde este sitio te permite interactuar para poder editar el perfil existente y asi mismo ingresar nuevas imagenes, dar "like" a las fotos y poder eliminarlas.
+
+Sitio Web: https://diegoogg.github.io/web_project_around/
---
+++ scripts/script.js
@@ -0,0 +1,67 @@
+// Busquemos el formulario en el DOM
+let formElement = document.querySelector(".popup__container");
+
+let newProfile = document.querySelector(".popup_container-profile");
+let newPlace = document.querySelector(".popup_container-place");
+
+let editProfile = document.querySelector(".profile__edit-btn");
+let addPlace = document.querySelector(".profile__add-btn");
+
+let placeInput = document.querySelector(".popup__input_place");
+let placeSrc = document.querySelector(".popup__input_src");
+
+let nameInput = document.querySelector(".popup__input_name");
+let jobInput = document.querySelector(".popup__input_job");
+
+let profileName = document.querySelector(".profile__name");
+let profileJob = document.querySelector(".profile__description");
+
+let closePlace = document.querySelector("#closeNewPlace");
+let closeProfile = document.querySelector("#closeEditProfile");
+
+function handleEditProfile(evt) {
+  evt.preventDefault();
+  newProfile.classList.add("popup_opened");
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
+
+function handleaddPlace(evt) {
+  evt.preventDefault();
+  newPlace.classList.add("popup_opened");
+}
+
+function handleCloseEditProfile(evt) {
+  evt.preventDefault();
+  newProfile.classList.remove("popup_opened");
+}
+
+function handleClosePlace(evt) {
+  newPlace.classList.remove("popup_opened");
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  newProfile.classList.remove("popup_opened");
+  // Obtén los valores de cada campo desde la propiedad de valor correspondiente
+
+  // Selecciona los elementos donde se introducirán los valores de los campos
+
+  // Inserta nuevos valores utilizando el textContent
+  // propiedad del método querySelector()
+}
+function handleNewPlaceFormSubmit(evt) {
+  evt.preventDefault();
+  newPlace.classList.remove("popup_opened");
+}
+// Conecta el manipulador (handler) al formulario:
+// se observará el evento de entrega
+
+formElement.addEventListener("submit", handleProfileFormSubmit);
+formElement.addEventListener("submit", handleNewPlaceFormSubmit);
+
+editProfile.addEventListener("click", handleEditProfile);
+addPlace.addEventListener("click", handleaddPlace);
+
+closePlace.addEventListener("click", handleClosePlace);
+closeProfile.addEventListener("click", handleCloseEditProfile);
